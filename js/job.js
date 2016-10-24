a51413698da7190122820d4f23f6aefadd6aabef
blinkme1 <blinkme1@ukr.net>
2016-10-24 18:44:27 +0300

    24/10/16 18:44:27

--- js/job.js
+++ js/job.js
@@ -5,7 +5,6 @@
  * @depends libraries/materialize.min.js
  */
 (function(global)
-
     $('textarea.material-input').autogrow({ horizontal: false, flickering: false});
     $(document).ready(function(){
         $('.collapsible').collapsible({
