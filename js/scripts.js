f0e95395fd7408eda66e6ad05ace9fdc28c810be
blinkme1 <blinkme1@ukr.net>
2016-11-01 14:55:08 +0200

    01/11/16 14:55:08

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,9 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+    ga('create', 'UA-85297028-1', 'auto');
+    ga('send', 'pageview');
+
     $(window).on("load", function() {
 
         $.fn.randomize = function(selector){
