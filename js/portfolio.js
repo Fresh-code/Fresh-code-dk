ad1500005139261683d6fafa9797ee14ae31c67c
artemsmart <artemarta78@gmail.com>
2016-11-14 18:31:38 +0200

    14/11/16 18:31:38

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,8 +5,7 @@
  * @depends libraries/list.min.js
  */
 (function(global) {
-
-
+    
     var $monitoring = $('.filter-monitoring'),
         $navigation = $('.filter-navigation'),
         $education = $('.filter-education'),
