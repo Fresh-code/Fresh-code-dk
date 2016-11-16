3ac237818c2a253b78bc2598915ce86168b0a966
blinkme1 <blinkme1@ukr.net>
2016-11-16 13:03:03 +0200

    using shell

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+require('shelljs/global');
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+    mkdir('-p', 'QQQQQQQQQQQQQ');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
