7225c662ef5b229b03b1442538d527619fb8dcb1
blinkme1 <blinkme1@ukr.net>
2016-11-17 17:50:04 +0200

    17/11/16 17:50:04

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ cd _site/
 # Commit and push last update to master branch
 git add .
 git commit -m "update site `date +"%d/%m/%y %H:%M:%S "`"
-git push origin master
+git push origin master --force
 
 # Purge all files in cloudflare
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/1c7a4eedbccff910e14ae06cc90a3622/purge_cache" \
