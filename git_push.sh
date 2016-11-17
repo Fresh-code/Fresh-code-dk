cf69c060b01a01bd10c52e32d3410f078d7586b1
blinkme1 <blinkme1@ukr.net>
2016-11-17 16:55:11 +0200

    17/11/16 16:55:11

--- git_push.sh
+++ git_push.sh
@@ -2,6 +2,10 @@
 
 # Commit and push to dev branch in gitlab
 
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+eval $(ssh-agent -s)
+ssh-add id_rsa
+
 git add .
 git commit -m "`date +"%d/%m/%y %H:%M:%S "`"
 git push gitlab-origin dev
